80af1a4098b21ad7e971f8e1e64c13e0bf4fad5c
ongjq98 <ongjq98@gmail.com>
2022-05-06 15:46:05 +0800

    owner 1.0.3

--- main.py
+++ main.py
@@ -84,12 +84,40 @@ def owner():
 @app.route("/owner/HourlyAvgSpending", methods=["GET", "POST"])
 def display_H_avg_spend():
     if request.method == "GET":
-        result = avgSpending(35,100,20)
-        return render_template("HourlySpending.html", totalHours=35, totalRevenue=100, totalCustomer=20, avgSpending=result)
+        return render_template("HourlySpending.html")
+    
+    else:
+        date_request = request.form["calendar"]
+        with psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host) as db:
+            with db.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+                cursor.execute(f"SELECT sum(total_amount) from cart where start_time between '{date_request} 12:00:00' and '{date_request} 17:59:59'")
+                totalRevenue = cursor.fetchall()
+            db.commit()
 
-def avgSpending(totalHours, totalRevnue, totalCustomer):
-    avg = totalRevnue/totalCustomer/totalHours
-    return avg
+        with psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host) as db:
+            with db.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+                cursor.execute(f"SELECT count(cart_id) from cart where start_time between '{date_request} 12:00:00' and '{date_request} 17:59:59'")
+                totalCustomer = cursor.fetchall()
+            db.commit()
+
+        print(totalRevenue)
+        print(totalCustomer)
+        
+        tr = [row[0] for row in totalRevenue]
+        tc = [row[0] for row in totalCustomer]
+        print(tr[0])
+        print(tc[0])
+        result = avgSpending(6,tr[0],tc[0])
+        return render_template("HourlySpending.html", totalHours=6, totalRevenue=totalRevenue, totalCustomer=totalCustomer, date_request = date_request, result = result)
+
+def avgSpending(totalHours, totalRevenue, totalCustomer):
+    avg = totalRevenue/totalCustomer/totalHours
+    return round(avg,2)
+
+
+
+
+
 
 @app.route("/owner/HourlyFrequency", methods=["GET", "POST"])
 def display_H_frequency():
@@ -97,8 +125,24 @@ def display_H_frequency():
         return render_template("HourlyFrequency.html")
 
     elif request.method == "POST":
-         birthday = request.form["birthday"]
-         return render_template("HourlyFrequency.html")
+        data=[
+            ("jello", 123)
+            ]
+
+
+
+
+        """db= psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
+        mycursor = db.cursor()
+        mycursor.execute("select count(cart_id) from cart where start_time between '2022-05-08 00:00:00' and '2022-05-08 23:59:59' ")
+        data = mycursor.fetchall()
+        mycursor.execute("select count(cart_id) from cart where start_time between '2022-05-08 00:00:00' and '2022-05-08 23:59:59' ")
+        data.extend(mycursor.fetchall()) """
+        labels = [row[0] for row in data]
+        values =[row[1] for row in data]
+        print(values)
+        date_request = request.form["calendar"]
+        return render_template("HourlyFrequency.html", date_request=date_request,values=values, labels=labels)
 
 #----End of Owner----#
 
