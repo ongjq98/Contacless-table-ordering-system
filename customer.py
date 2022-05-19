8ae6a47bbf29919dd76c202d6fc79e90132a7cc0
yongqin123 <66666937+yongqin123@users.noreply.github.com>
2022-05-19 20:38:10 +0800

    Add files via upload

--- customer.py
+++ customer.py
@@ -18,28 +18,108 @@ app.secret_key = "duck_rice"
 app.permanent_session_lifetime = timedelta(minutes=60)
 
 
-### LOGIN PAGE ###
+### CUSTOMER HOMEPAGE ###
 @app.route("/", methods=["GET", "POST"])
 def index():
-    boundary = CustomerAddOrderPage()
+    boundary = CustomerPage()
     if request.method == "GET":
-        return boundary.loginTemplate() # A-B 
+        print("Customer Home Page")
+        return boundary.customerHomePage() # A-B 
 
     elif request.method == "POST":
-        if boundary.controller.getOrderlist(request.form, request.form.getlist): # B-C, C-E
+        print("12345")
+        return boundary.buttonClicked(request.form) 
 
-            # login success - add username & account_type in session
-            #session["username"] = request.form["username"]
-            #session["account_type"] = request.form["type"]
+### CUSTOMER ADD ORDER ###
+@app.route("/add_order", methods=["GET", "POST"])
+def add_order():
+    boundary = CustomerPage()
+    if request.method == "GET":
+        print("Customer Add Order Page")
+        boundary_menu =  CustomerPage()
+        menu = boundary_menu.controller.getMenu()
+        return boundary.addOrderPage(menu) 
 
-            # redirect page to manager, staff, owner or admin
-            #return LoginPage.redirectPage(session["account_type"]) # C-B
-            print("managed to insert_data")
+    elif request.method == "POST":
+        if "return" not in request.form:
+            print("ADD ORDER FORM submitted")
+            boundary.controller.getOrderlistToAdd(request.form, request.form.getlist) # B-C, C-E
+            session["cartId"] = boundary.controller.entity.cart_id
+            session["tableId"] = boundary.controller.entity.table_id
+            session["phone_no"] = boundary.controller.entity.phone_no
+            return boundary.redirectToCustomerPage()
         else:
+            if request.form["return"] == "return":
+                print("return pressed")
+                return redirect(url_for("index"))
+       
             
-            return boundary.loginTemplate() # redirect to login page
+        
 
-    
+### EDIT PAGE ###
+@app.route("/editOrder", methods=["GET", "POST"])
+def editOrder():  
+    boundary2 = CustomerPage()
+    if request.method == "GET":
+        print("Edit ORDER")
+        boundary_menu =  CustomerPage()
+        menu = boundary_menu.controller.getMenu()
+        return boundary2.editOrderPage(menu) 
+
+    elif request.method == "POST":
+        if "return" not in request.form:
+            boundary2.controller.entity.cart_id = session["cartId"]
+            boundary2.controller.entity.table_id = session["tableId"]
+            boundary2.controller.entity.phone_no = session["phone_no"]
+            boundary2.controller.getOrderlistToUpdateAndAdd(request.form, request.form.getlist)
+            return boundary2.redirectToCustomerPage()
+        else:
+            if request.form["return"] == "return":
+                print("return pressed")
+                return redirect(url_for("index"))
+
+### DELETE ORDER PAGE ###
+@app.route("/deleteOrder", methods=["GET", "POST"])
+def deleteOrder():  
+    boundary3 = CustomerPage()
+    if request.method == "GET":
+        print("Delete ORDER")
+        return boundary3.deleteOrderPage() # A-B 
+
+    elif request.method == "POST":
+        if "return" not in request.form:
+            boundary3.controller.entity.cart_id = session["cartId"]
+            boundary3.controller.entity.table_id = session["tableId"]
+            boundary3.controller.entity.phone_no = session["phone_no"]
+            boundary3.controller.getOrderlistToDelete(request.form.getlist)
+            return boundary3.redirectToCustomerPage()
+        else:
+            if request.form["return"] == "return":
+                print("return pressed")
+                return redirect(url_for("index"))
+                
+### PAYMENT ORDER PAGE ###
+@app.route("/payment", methods=["GET", "POST"])
+def payment():  
+    boundary4 = CustomerPage()
+    if request.method == "GET":
+        print("PAYMENT ORDER")
+        return boundary4.deleteOrderPage() # A-B 
+
+    elif request.method == "POST":
+        boundary4.controller.entity.cart_id = session["cartId"]
+        boundary4.controller.entity.table_id = session["tableId"]
+        boundary4.controller.entity.phone_no = session["phone_no"]
+        boundary4.controller.getpaymentDetails(request.form)
+        return boundary4.redirectToCustomerPage()
+
+
+### VIEW Menu ###
+@app.route("/viewMenu", methods=["GET", "POST"])
+def viewMenu():
+    pass
+
+
 
 ### INITIALIZATION ###
 if __name__ == "__main__":
